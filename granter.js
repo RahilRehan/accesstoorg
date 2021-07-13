527a02207ba9b2199a2f0e2e2a9ff28eff36e745
Rahil Rehan <rahil.rehan@thoughtworks.com>
2021-07-13 16:36:11 +0530

    [Rahil] Add. Octokit and give access to orgs

--- granter.js
+++ granter.js
@@ -1,22 +1,74 @@
-var request = require('request');
+const { Octokit } = require("@octokit/rest");
+const express = require("express")
 
-var headers = {
-    'Accept': 'application/vnd.github.v3+json',
-    'Authorization': `token ${GH_TOKEN}`,
-    'user-agent': 'none'
-};
+const app = express()
+require('dotenv').config()
 
-var options = {
-    url: 'https://api.github.com/orgs/rahil-rehan/memberships/RahilRehan',
-    method: 'PUT',
-    headers: headers
-};
+const octokit = new Octokit({
+    auth: process.env.CATALYST_ADMIN_TOKEN
+});
 
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body);
+const ORGANIZATION = process.env.ORGANIZATION
+const PORT = 9999 || process.env.PORT
+
+
+// retrieve id from github username, useful when adding user to teams via github api
+const getUserIdFromUserName = async (userName) => {
+    try{
+        const response = await octokit.request(`GET /users/${userName}`)
+        return Number(response.data.id)
     }
-    console.log(response.body)
+    catch(err){
+        console.log(err.response.status, err.response.data)
+    }
+} 
+
+//get all teams associated with github token
+const getAllTeams = async () => {
+    try{
+        const response = await octokit.request(`GET /orgs/${ORGANIZATION}/teams`)
+        const teams = response.data
+        console.table(
+            teams.map(team => {
+                return {
+                "Team": team.id,
+                "Name": team.name
+                }
+            })
+        )
+    }
+    catch(err){
+        console.log(err.response.status, err.response.data)
+    }
 }
 
-request(options, callback);
+// gives access to userid with given teams array
+const giveTeamsAccess = async (userId, team_ids) => {
+    try{
+        const response = await octokit.request(`POST /orgs/${ORGANIZATION}/invitations`, {
+            invitee_id: userId,
+            team_ids : team_ids
+        })
+        console.log(response.data)
+    }
+    catch(err){
+        console.log(err.response.status, err.response.data)
+    }
+}
+
+//bulk add users to catalyst
+addMultipleUsersToCatalyst = () => {
+    const userIds = [58245868] //list of users
+    const teamIds = [4064135] //list of teams to give access to
+    userIds.forEach(userId => {
+        giveTeamsAccess(userId, teamIds)
+    })
+}
+
+//REST API
+app.get('/addUser', async (req, res) => {
+    const userId = await getUserIdFromUserName(req.query.username)
+    giveTeamsAccess(userId, [Number(process.env.TW_EXPLORER_ID)])
+})
+
+app.listen(PORT, () => console.log(`Giving access from ${PORT}`))
